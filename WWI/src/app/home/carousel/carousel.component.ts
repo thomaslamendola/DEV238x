4f6876b392ebd1c61dba5dddc64c00b503efffbd
Thomas La Mendola <thomas.lamendola@gmail.com>
2019-04-12 06:37:07 +0200

    Carousel static displaying sorted

--- WWI/src/app/home/carousel/carousel.component.ts
+++ WWI/src/app/home/carousel/carousel.component.ts
@@ -18,7 +18,9 @@ export class CarouselComponent implements OnInit {
     this.dataService.getProducts().subscribe(productCategories => {
       let subCategories = productCategories.map(category => category.subcategories).reduce((a, b) => a.concat(b), []);
       let items = subCategories.map(subCategory => subCategory.items).reduce((a, b) => a.concat(b), []);
-      this.imageList = items.map(item => item.imagelink).slice(0, 12);
+      let imageLinks = items.map(item => item.imagelink);
+      this.shuffleArray(imageLinks);
+      this.imageList = imageLinks.slice(0, 12);
     });
   }
 
@@ -34,10 +36,19 @@ export class CarouselComponent implements OnInit {
     this.showSlides(this.slideIndex = n);
   }
 
+  shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
   showSlides(n: number) {
     var i: number;
     var slides = document.getElementsByClassName("slide");
-    if(slides.length === 0) return;
+    if (slides.length === 0) return;
     var pages = document.getElementsByClassName("page");
     if (n > slides.length) { this.slideIndex = 1 }
     if (n < 1) { this.slideIndex = slides.length }
