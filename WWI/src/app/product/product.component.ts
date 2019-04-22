f1b72e3eb504b50d461c4174cb85d03180904997
Thomas La Mendola <thomas.lamendola@gmail.com>
2019-04-22 15:37:09 +0200

    added routing to product with not found fallback

--- WWI/src/app/product/product.component.ts
+++ WWI/src/app/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { Item, ProductCategory } from '../core/models/product-category';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  private sub: any;
+  item: Item = null;
+  found: Boolean = false;
+  categories: ProductCategory[] = [];
+
+  constructor(private route: ActivatedRoute, private dataService: DataService, private router: Router) { }
 
   ngOnInit() {
+    this.dataService.getProducts().subscribe(categories => {
+      this.categories = categories;
+    });
+
+    this.sub = this.route.params.subscribe(params => {
+      this.id = params['id'];
+      this.categories.forEach(cat => {
+        cat.subcategories.forEach(sub => {
+          sub.items.forEach(item => {
+            if (item.name === this.id) {
+              this.item = item;
+              this.found = true;
+            }
+          });
+        });
+      });
+      if (!this.found) {
+        this.router.navigate(['/not-found']);
+      }
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
